61eb609837efca13cfb920d1ab6bdde3ee680a21
Alix Tiran-Cappello <alix.tiran-cappello@laposte.net>
2021-08-24 14:37:42 +0200

    finished refactoring

--- zytholic_project/style_rename.py
+++ zytholic_project/style_rename.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import ipdb
 
 def style_rename():
     # importation des datasets
@@ -17,21 +16,20 @@ def style_rename():
     
     # importation du fichier de corresponance des styles
     style_xls = pd.read_excel('assets/style_convert.xlsx')
-    style_xls.to_csv("raw_data/style_rename.csv")
+    #style_xls.to_csv("raw_data/style_rename.csv")
 
-    style_csv = pd.read_csv('raw_data/style_rename.csv')
-    style_csv.drop(columns='Unnamed: 0', inplace=True)
-    style_csv.set_index('Dataset style', inplace=True)
-    style_csv = style_csv.iloc[1:, :]
+    #style_csv = pd.read_csv('raw_data/style_rename.csv')
+    #style_xls.drop(columns='Unnamed: 0', inplace=True)
+    style_xls.set_index('Dataset style', inplace=True)
+    style_xls = style_xls.iloc[1:, :]
 
     # creation d'un dict pour replace de la correspondance
-    style_dict = style_csv.to_dict()
+    style_dict = style_xls.to_dict()
     style_dict = style_dict['Unnamed: 1']
 
     # uniformaisation des styles sur les datasets
     beers['style'].replace(style_dict, inplace=True)
     top_beer_info['style'].replace(style_dict, inplace=True)
-    #ipdb.set_trace()
 
     # exportation en csv des datasets modifiés
     top_beer_info.to_csv('raw_data/top_beer_info_style_renamed.csv')
@@ -39,6 +37,5 @@ def style_rename():
     
 
 if __name__ == '__main__':
-        #ipdb.set_trace()
         style_rename()
         print('Files saved after style renaming')
