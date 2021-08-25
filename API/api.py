ea74d2e0807a2a906a7fd8cbd895947a3f840cd8
stephane THOMAS <stephane.thomas75@gmail.com>
2021-08-25 09:33:50 +0200

    endpoint creation and test OK

--- API/api.py
+++ API/api.py
@@ -5,6 +5,16 @@ app = FastAPI()
 # define a root `/` endpoint
 @app.get("/")
 def index():
-    return {"ok": True}
+    return {"Zytholic API : running": True}
 
+@app.get("/10_prefered_beers")
+def index():
+    return {"Zytholic API : return 10 beers": True}
 
+@app.get("/brewery")
+def index():
+    return {"Zytholic API : return brewery next to you": True}
+
+@app.get("/test")
+def index():
+    return {"Zytholic API : test": True}
