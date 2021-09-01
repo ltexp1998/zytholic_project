a8e8c385abe2992c04782b23ace3a624feee0432
Alix Tiran-Cappello <alix.tiran-cappello@laposte.net>
2021-09-01 10:19:34 +0200

    Added pickle model

--- zytholic_project/base_model.py
+++ zytholic_project/base_model.py
@@ -24,12 +24,12 @@ class BaseModel():
 
     def get_data(self):
         """Merge data from Beers and Breweries using top-information as reference"""
-        dfbrew = pd.read_csv("../raw_data/Beers_Breweries_and_Beer Reviews/breweries.csv")
-        dfbeer = pd.read_csv("../raw_data/beers_style_renamed.csv")
-        dftop = pd.read_csv("../raw_data/top_beer_info_style_renamed.csv")
+        dfbrew = pd.read_csv("raw_data/Beers_Breweries_and_Beer Reviews/breweries.csv")
+        dfbeer = pd.read_csv("raw_data/beers_style_renamed.csv")
+        dftop = pd.read_csv("raw_data/top_beer_info_style_renamed.csv")
 
         #read correspondance brewery
-        corres_xls = pd.read_csv('../assets/matching_brewery_names.csv')
+        corres_xls = pd.read_csv('assets/matching_brewery_names.csv')
         corres_xls.set_index('bbr', inplace=True)
         corres= corres_xls.to_dict()
 
@@ -61,7 +61,7 @@ class BaseModel():
         """
 
         # Get matching table for styles names and format it
-        style_csv = pd.read_csv('../assets/style_convert.csv')
+        style_csv = pd.read_csv('assets/style_convert.csv')
         style_csv = style_csv[['Converted', 'Simplified']]
 
         # creation of a dictionary to replace automatically
@@ -133,6 +133,7 @@ class BaseModel():
         with open("assets/model.pkl", "wb") as file:
             pickle.dump(self, file)
         print('Model saved')
+        pass
         
     def save_calculated_distances(self):
         """"""
@@ -143,6 +144,7 @@ class BaseModel():
         linear_sim = linear_kernel(self.X, self.X)
         pd.DataFrame(linear_sim).to_csv('assets/linear.csv', index=False)
         print('Datafile with similarity distances saved')
+        pass
 
     
 if __name__ == '__main__':
