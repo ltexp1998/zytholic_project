66872497e861a514ffed35a5babbf23c126f2a5b
Alix Tiran-Cappello <alix.tiran-cappello@laposte.net>
2021-10-17 11:05:04 +0200

    Added test for fastapi style search

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,6 +15,17 @@ def test_filter_abv_ibu():
     response = client.get(call)
     assert response.status_code == 200
     
+def test_style_api():
+    call = f'/style_search?style="Stout"&abv={8}&ibu={100}'
+    response = client.get(call)
+    assert response.status_code == 200
+    all = f'/style_search?style="Lager"&abv={4}&ibu={35}'
+    response = client.get(call)
+    assert response.status_code == 200
+    all = f'/style_search?style="IPA"&abv={5}&ibu={20}'
+    response = client.get(call)
+    assert response.status_code == 200
+    
 if __name__ == '__main__':
     print(test_basic_name())
     print(test_filter_abv_ibu())
