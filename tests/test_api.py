1860dcc5e755764c8ce5557b9edba78c1c08c9c1
Alix Tiran-Cappello <alix.tiran-cappello@laposte.net>
2021-10-21 19:02:54 +0200

    Added country check for API"

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,15 +14,21 @@ def test_filter_abv_ibu():
     call = f'/filter_abv_ibu?name={beer_name}&abv={8}&ibu={100}'
     response = client.get(call)
     assert response.status_code == 200
+    call = f'/filter_abv_ibu?name={beer_name}&abv={8}&ibu={100}&input_country={"FR"}'
+    response = client.get(call)
+    assert response.status_code == 200
     
 def test_style_api():
     call = f'/style_search?style="Stout"&abv={8}&ibu={100}'
     response = client.get(call)
     assert response.status_code == 200
-    all = f'/style_search?style="Lager"&abv={4}&ibu={35}'
+    call = f'/style_search?style="Lager"&abv={4}&ibu={35}'
     response = client.get(call)
     assert response.status_code == 200
-    all = f'/style_search?style="IPA"&abv={5}&ibu={20}'
+    call = f'/style_search?style="IPA"&abv={5}&ibu={20}'
+    response = client.get(call)
+    assert response.status_code == 200
+    call = f'/style_search?style="Stout"&input_coutry="FR"'
     response = client.get(call)
     assert response.status_code == 200
     
